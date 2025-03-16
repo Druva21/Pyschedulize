8013915cc10de8115550e3380a48d592b607c3cf
Druva21 <gorladruvakumar@gmail.com>
2025-03-17 03:48:36 +0530

    push

--- app.py
+++ app.py
@@ -227,4 +227,4 @@ def generate_timetable():
         #     return ('error processing form data. error is'+str(e))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
